8a12f22b74fd09d508f7bae6bfd600bead8e40c1
Poonam Borkar <poonam.deshmukh@gmail.com>
2020-05-14 19:21:06 +0530

    staticfix

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -118,4 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+SITE_ROOT = os.path.abspath(os.path.dirname(__file__))
+...
 STATIC_URL = '/static/'
+...
+STATIC_ROOT = os.path.join( SITE_ROOT, 'static')
+SITE_STATIC_ROOT = os.path.join( SITE_ROOT, 'local_static')
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Don't forget to use absolute paths, not relative paths.
+    ('', SITE_STATIC_ROOT),
+)
